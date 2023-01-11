9a885bc111811ec04b8e6b07e918ee79d2a5ff02
beatlec <53367946+beatlec@users.noreply.github.com>
2023-01-11 16:24:13 +0800

    Update methods.go

--- builtin/v9/verifreg/methods.go
+++ builtin/v9/verifreg/methods.go
@@ -2,8 +2,8 @@ package verifreg
 
 import (
 	"github.com/beatlec/filecoin-project-go-address"
-	"github.com/beatlec/filecoin-project-go-state-types/abi"
-	"github.com/beatlec/filecoin-project-go-state-types/builtin"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/builtin"
 )
 
 var Methods = map[abi.MethodNum]builtin.MethodMeta{
