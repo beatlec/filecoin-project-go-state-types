55dcfc4685320d7ed9b98da2dc0a61336ebd2caa
beatlec <53367946+beatlec@users.noreply.github.com>
2023-01-11 11:30:01 +0800

    update filecoin-project-go-state-types

--- builtin/v9/market/invariants.go
+++ builtin/v9/market/invariants.go
@@ -4,16 +4,16 @@ import (
 	"bytes"
 	"encoding/binary"
 
-	"github.com/filecoin-project/go-state-types/builtin/v9/verifreg"
+	"github.com/beatlec/filecoin-project-go-state-types/builtin/v9/verifreg"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/beatlec/filecoin-project-go-state-types/abi"
+	"github.com/beatlec/filecoin-project-go-state-types/big"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/go-state-types/builtin"
-	"github.com/filecoin-project/go-state-types/builtin/v9/util/adt"
+	"github.com/beatlec/filecoin-project-go-state-types/builtin"
+	"github.com/beatlec/filecoin-project-go-state-types/builtin/v9/util/adt"
 )
 
 type DealSummary struct {
